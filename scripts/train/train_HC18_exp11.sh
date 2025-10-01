9ca96fb06139f57f74f73a3cdf7eed13cdf33702
Jason0411202 <Jason0411202@gmail.com>
2025-10-01 20:33:29 +0800

    exp11 train longer

--- scripts/train/train_HC18_exp11.sh
+++ scripts/train/train_HC18_exp11.sh
@@ -1,8 +1,8 @@
 # 實驗十一
 # 在不同 degradation 上, 測試引入 GAN loss 造成的影響
 
-TRAIN_DEVICES="5,7"
-TEST_DEVICE="5"
+TRAIN_DEVICES="0,1"
+TEST_DEVICE="0"
 PROJECT_NAME="pisasr-exp11"
 EXP="exp11"
 
@@ -21,6 +21,8 @@ for i in "${!degradations[@]}"; do
         --train_folder="src/datasets/for_generate_dataset/outputs/${degradation}/training_set/GT" \
         --train_folder_lr="src/datasets/for_generate_dataset/outputs/${degradation}/training_set/LR" \
         --output_dir="experiments/HC18/${EXP}/${exp_id}/train" \
+        --pix_steps 15000 \
+        --max_train_steps 31000 \
         --enable_gan_loss=${gan_loss} \
         --wandb_project_name "${PROJECT_NAME}" \
         --wandb_run_name "${EXP}_${degradation}_enable_gan_loss${gan_loss} (train)"
@@ -30,7 +32,7 @@ for i in "${!degradations[@]}"; do
     python test_pisasr.py \
         --input_gt_image src/datasets/for_generate_dataset/outputs/${degradation}/test_set/GT \
         --input_lr_image src/datasets/for_generate_dataset/outputs/${degradation}/test_set/LR \
-        --pretrained_path experiments/HC18/${EXP}/${exp_id}/train/checkpoints/model_1001.pkl \
+        --pretrained_path experiments/HC18/${EXP}/${exp_id}/train/checkpoints/model_30001.pkl \
         --output_dir experiments/HC18/${EXP}/${exp_id}/test \
         --wandb_project_name "${PROJECT_NAME}" \
         --wandb_run_name "${EXP}_${degradation}_enable_gan_loss${gan_loss} (test)"
